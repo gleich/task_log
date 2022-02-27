ebd8baddb36ef83f1ef868e7002c7586bafc5d75
Matt Gleich <git@mattglei.ch>
2022-02-27 00:32:45 -0500

    feat: configuration
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,6 +1,6 @@
 use task_log::task;
 
 fn main() {
-    let sum = task("Adding 1 and 2", || -> u32 { 1 + 2 });
-    println!("Answer is {}", sum);
+    let result = task("Adding two numbers", || -> u32 { 1 + 2 });
+    println!("{}", result);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,74 @@
-use std::fmt::Display;
+use std::{
+    fmt::Display,
+    sync::{Arc, Mutex, MutexGuard, PoisonError},
+};
 
-use colorful::Colorful;
+mod util;
+
+use chrono::Utc;
+use colorful::{core::color_string::CString, Colorful};
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref CONF: Arc<Mutex<Conf>> = Arc::new(Mutex::new(Conf::default()));
+}
+
+pub struct Conf {
+    pub color: bool,
+}
+
+impl Conf {
+    pub fn apply<'a>(self) -> Result<(), PoisonError<MutexGuard<'a, Conf>>> {
+        let mut changer = CONF.lock()?;
+        *changer = self;
+        Ok(())
+    }
+}
+
+impl Default for Conf {
+    fn default() -> Conf {
+        Conf { color: true }
+    }
+}
 
 pub fn task<S, F, R>(name: S, mut runner: F) -> R
 where
     F: FnMut() -> R,
     S: Display,
 {
-    println!("  {}  | {}", "RUNNING".yellow(), name);
+    let arc_ref = Arc::clone(&CONF);
+    let config = arc_ref.lock().unwrap();
+
+    // START
+    let start_time = Utc::now();
+    let running_msg = "  RUNNING       ";
+    println!(
+        "{}| {}",
+        if config.color {
+            running_msg.yellow()
+        } else {
+            CString::new(running_msg)
+        },
+        name
+    );
+
     let result = runner();
+
+    // DONE
     println!("\x1b[A\x1b[A");
-    println!("  {}     | {}", "DONE".green(), name);
+    let done_msg = format!(
+        "  DONE in {}  ",
+        util::format_duration(Utc::now() - start_time)
+    );
+    println!(
+        "{} | {}",
+        if config.color {
+            done_msg.green()
+        } else {
+            CString::new(done_msg)
+        },
+        name
+    );
     result
 }
 
